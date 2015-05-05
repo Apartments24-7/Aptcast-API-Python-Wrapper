623acc431094092bf317223e53d95a0742f000f5
Chris Jones <chris@brack3t.com>
2015-05-05 16:21:44 -0700

    Updated corporation create method. Still need to figure out how to handle the corporation id header.

--- aptcast/corporation.py
+++ aptcast/corporation.py
@@ -4,24 +4,25 @@ from api import Resource
 
 
 class Corporation(Resource):
+    base_action = "corp"
     app = "corporation"
 
     def __init__(self, api):
         self.api = api
 
-    def create(self, name, email, website, phone, address, city, state,
-               postal_code):
-
+    def create(self, name, address0, address1, city, state, postal_code,
+               email, website, phone, parent):
+        self.action = self.base_action
         data = {
-            "corporation": {
-                "name": name,
-                "contact": {
-                    "email": email,
-                    "website": website,
-                    "phone": phone
-                },
+            "name": name,
+            "parent": parent,
+            "contact": {
+                "email": email,
+                "website": website,
+                "phone": phone,
                 "address": {
-                    "address0": address,
+                    "address0": address0,
+                    "address1": address1,
                     "city": city,
                     "state": state,
                     "postal_code": postal_code
@@ -29,4 +30,5 @@ class Corporation(Resource):
             }
         }
 
-        return self.api.post("corporation", "create", params=json.dumps(data))
+        return self.api.post(
+            self.get_app(), self.get_action(), params=json.dumps(data))
