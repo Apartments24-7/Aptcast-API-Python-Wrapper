13832021a87d14d4eda7dbfa5f7e6b83b521af28
Chris Jones <chris@brack3t.com>
2015-05-06 12:18:35 -0700

    added update method but it appears it's not yet implemented in the API. Commenting out for now.

--- aptcast/corporation.py
+++ aptcast/corporation.py
@@ -32,3 +32,26 @@ class Corporation(Resource):
 
         return self.api.post(
             self.get_app(), self.get_action(), params=json.dumps(data))
+
+    # def update(self, corporation_id, name, address0, address1, city, state,
+    #            postal_code, email, website, phone):
+    #     self.action = "{0}/{1}".format(self.base_action, corporation_id)
+    #
+    #     data = {
+    #         "name": name,
+    #         "contact": {
+    #             "email": email,
+    #             "website": website,
+    #             "phone": phone,
+    #             "address": {
+    #                 "address0": address0,
+    #                 "address1": address1,
+    #                 "city": city,
+    #                 "state": state,
+    #                 "postal_code": postal_code
+    #             }
+    #         }
+    #     }
+    #
+    #     return self.api.put(
+    #         self.get_app(), self.get_action(), params=json.dumps(data))
