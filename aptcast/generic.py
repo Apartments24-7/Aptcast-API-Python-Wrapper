f52b39960ac40a381af4eb23ef30c1bd52f86f63
Chris Jones <chris@brack3t.com>
2015-05-06 22:09:41 -0700

    base amenity resource updated.

--- aptcast/generic.py
+++ aptcast/generic.py
@@ -1,32 +1,24 @@
-import json
-
 from api import Resource
 
 
-class GenericResource(Resource):
+class BaseAmenityResource(Resource):
     app = "generic"
-
-
-class BaseAmenityResource(GenericResource):
     base_action = "base-amenities"
 
     def create(self, name):
         self.action = self.base_action
         data = {"name": name}
 
-        return self.api.post(
-            self.get_app(), self.get_action(), params=json.dumps(data))
+        return self.api.post(self.get_app(), self.get_action(), params=data)
 
-    def update(self, amenity_id, name):
-        self.action = "{0}/{1}".format(self.base_action, amenity_id)
-        data = {"id": amenity_id, "name": name}
+    def update(self, aptcast_id, name):
+        self.action = "{0}/{1}".format(self.base_action, aptcast_id)
+        data = {"id": aptcast_id, "name": name}
 
-        return self.api.put(
-            self.get_app(), self.get_action(), params=json.dumps(data))
+        return self.api.put(self.get_app(), self.get_action(), params=data)
 
-    def delete(self, amenity_id):
-        self.action = "{0}/{1}".format(self.base_action, amenity_id)
-        data = {"id": amenity_id}
+    def delete(self, aptcast_id):
+        self.action = "{0}/{1}".format(self.base_action, aptcast_id)
+        data = {"id": aptcast_id}
 
-        return self.api.delete(
-            self.get_app(), self.get_action(), params=json.dumps(data))
+        return self.api.delete(self.get_app(), self.get_action(), params=data)
