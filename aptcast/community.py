4670b3d74759613a45cdfc67ba32e5baa514d667
Christopher Lien <cjlien42@gmail.com>
2015-05-22 15:03:33 -0700

    Some clean up in the communities app

--- aptcast/community.py
+++ aptcast/community.py
@@ -1,4 +1,3 @@
-import json
 import requests
 
 from api import Resource
@@ -9,76 +8,89 @@ class CommunityResource(Resource):
     app = "community"
     base_action = "communities"
 
-    def create(self, name, lat, lng, address0, address1, city, state,
-               postal_code, email, website, phone, pet_policy_details, dogs,
-               cats, senior, section8, student, corporate, military,
-               corporation, description, logo_description,
-               hero_shot_description):
+    def build_address(self, address0, city, state, postal_code, address1=""):
+        address = {
+            "address0": address0,
+            "city": city,
+            "state": state,
+            "postal_code": postal_code
+        }
+        if address1:
+            address["address1"] = address1
+        return address
+
+    def build_contact(self, phone, email="", website="", **kwargs):
+        contact = {
+            "phone": phone,
+            "address": self.build_address(**kwargs)
+        }
+        if email:
+            contact["email"] = email
+        if website:
+            contact["website"] = website
+        return contact
+
+    def build_community(self, name, lat, lng, phone, address0, city, state,
+                        postal_code, description="", address1="", email="",
+                        website="", **kwargs):
+        """
+        Create Aptcast Community Dict
+
+        Required arguments include:
+            name: <string>,
+            lat: <float>,
+            lng: <float>,
+            phone: <string>,
+            address0: <string>,
+            city: <string>,
+            state: <string>,
+            postal_code: <string|integer>
+        Optional arguments include:
+            description: <string>,
+            address1: <string>,
+            email: <string>,
+            website: <string>,
+            pet_policy_details: <string>,
+            dogs: <boolean>,
+            cats: <boolean>,
+            senior: <boolean>,
+            section8: <boolean>,
+            student: <boolean>,
+            corporate: <boolean>,
+            military: <boolean>
+        """
         self.action = self.base_action
 
         data = {
-            "description": {"body": description},
             "name": name,
             "location": {
                 "lat": lat,
                 "lng": lng
             },
-            "contact": {
-                "email": email,
-                "website": website,
-                "phone": phone,
-                "address": {
-                    "address0": address0,
-                    "address1": address1,
-                    "city": city,
-                    "state": state,
-                    "postal_code": postal_code,
-                }
-            },
-            "pet_policy": {
-                "cats": cats,
-                "dogs": dogs,
-                "details": pet_policy_details or ""
-            }
+            "contact": self.build_contact(
+                phone, email, website, address0=address0, address1=address1,
+                city=city, state=state, postal_code=postal_code
+            )
         }
 
+        if description == "":
+            description = None
+        data["description"] = description
+
+        for key, value in kwargs.items():
+            data[key] = value
+
+        return data
+
+    def create(self, *args, **kwargs):
+        data = self.build_community(*args, **kwargs)
         return self.api.post(
             self.get_app(), self.get_action(), params=data)
 
-    def update(self, aptcast_id, name, lat, lng, address0, address1, city,
-               state, postal_code, email, website, phone, pet_policy_details,
-               dogs, cats, senior, section8, student, corporate, military,
-               corporation, description, logo_description,
-               hero_shot_description):
+    def update(self, aptcast_id, *args, **kwargs):
         self.action = "{0}/{1}".format(self.base_action, aptcast_id)
-
-        data = {
-            "description": {"body": description},
-            "name": name,
-            "location": {
-                "lat": lat,
-                "lng": lng
-            },
-            "contact": {
-                "email": email,
-                "website": website,
-                "phone": phone,
-                "address": {
-                    "address0": address0,
-                    "address1": address1,
-                    "city": city,
-                    "state": state,
-                    "postal_code": postal_code,
-                }
-            },
-            "pet_policy": {
-                "cats": cats,
-                "dogs": dogs,
-                "details": pet_policy_details or ""
-            }
-        }
-        return self.api.put(
-            self.get_app(), self.get_action(), params=data)
+        data = self.build_community(*args, **kwargs)
+        return self.api.put(self.get_app(), self.get_action(), params=data)
 
     def delete(self, aptcast_id):
         self.action = "{0}/{1}".format(self.base_action, aptcast_id)
@@ -113,75 +125,49 @@ class FloorPlanResource(Resource):
     app = "community"
     base_action = "floor-plan"
 
-    def create(self, community_aptcast_id, beds, baths, name, is_loft,
-               is_studio, description, rent_low,
-               rent_high, deposit_low, deposit_high, square_feet_low,
-               square_feet_high):
-        self.action = "{0}/{1}".format(community_aptcast_id, self.base_action)
+    def build_low_high(self, low, high=None):
+        return {
+            "low": low,
+            "high": high
+        }
 
-        description_dict = None
-        if description:
-            description_dict = {
-                "body": description
-            }
-
+    def build_floor_plan(self, beds, baths, name, rent_low, deposit_low,
+                         square_feet_low, is_loft=False, is_studio=False,
+                         rent_high=None, deposit_high=None,
+                         square_feet_high=None, description=None):
         data = {
             "name": name,
             "beds": beds,
             "baths": baths,
-            "description": description_dict,
-            "rent": {
-                "low": rent_low,
-                "high": rent_high
-            },
-            "deposit": {
-                "low": deposit_low,
-                "high": deposit_high
-            },
-            "square_feet": {
-                "low": square_feet_low,
-                "high": square_feet_high
-            },
+            "rent": self.build_low_high(rent_low, rent_high),
+            "deposit": self.build_low_high(deposit_low, deposit_high),
+            "square_feet": self.build_low_high(
+                square_feet_low,
+                square_feet_high
+            ),
             "is_loft": is_loft,
             "is_studio": is_studio
         }
+
+        if description == "":
+            description = None
+        data["description"] = description
+
+        return data
+
+    def create(self, community_aptcast_id, *args, **kwargs):
+        self.action = "{0}/{1}".format(community_aptcast_id, self.base_action)
+
+        data = self.build_floor_plan(*args, **kwargs)
 
         return self.api.post(
             self.get_app(), self.get_action(), params=data)
 
-    def update(self, community_aptcast_id, aptcast_id, beds, baths, name,
-               is_loft, is_studio, description, rent_low, rent_high,
-               deposit_low, deposit_high, square_feet_low, square_feet_high):
-
-        description_dict = None
-        if description:
-            description_dict = {
-                "body": description
-            }
-
-        data = {
-            "name": name,
-            "beds": beds,
-            "baths": baths,
-            "description": description_dict,
-            "rent": {
-                "low": rent_low,
-                "high": rent_high
-            },
-            "deposit": {
-                "low": deposit_low,
-                "high": deposit_high
-            },
-            "square_feet": {
-                "low": square_feet_low,
-                "high": square_feet_high
-            },
-            "is_loft": is_loft,
-            "is_studio": is_studio
-        }
-
+    def update(self, community_aptcast_id, aptcast_id, *args, **kwargs):
         self.action = "{0}/{1}/{2}".format(
             community_aptcast_id, self.base_action, aptcast_id)
+
+        data = self.build_floor_plan(*args, **kwargs)
 
         return self.api.put(
             self.get_app(), self.get_action(), params=data)
@@ -225,71 +211,47 @@ class UnitResource(Resource):
     app = "community"
     base_action = "units"
 
-    def create(self, floor_plan_aptcast_id, number, building, floor,
-               available_date, description, rent_low, rent_high, deposit_low,
-               deposit_high, square_feet_low, square_feet_high):
-        self.action = "{0}/{1}".format(floor_plan_aptcast_id, self.base_action)
+    def build_low_high(self, low, high=None):
+        return {
+            "low": low,
+            "high": high
+        }
 
-        description_dict = None
-        if description:
-            description_dict = {
-                "body": description
-            }
-
+    def build_unit(self, rent_low, deposit_low, square_feet_low,
+                   rent_high=None, deposit_high=None, square_feet_high=None,
+                   number="", building="", floor="", available_date=None,
+                   description=None):
         data = {
             "number": number,
             "building": building,
             "floor": floor,
             "available_date": available_date,
-            "description": description_dict,
-            "rent": {
-                "low": rent_low,
-                "high": rent_high
-            },
-            "deposit": {
-                "low": deposit_low,
-                "high": deposit_high
-            },
-            "square_feet": {
-                "low": square_feet_low,
-                "high": square_feet_high
-            }
+            "rent": self.build_low_high(rent_low, rent_high),
+            "deposit": self.build_low_high(deposit_low, deposit_high),
+            "square_feet": self.build_low_high(
+                square_feet_low,
+                square_feet_high
+            ),
         }
+
+        if description == "":
+            description = None
+        data["description"] = description
+
+        return data
+
+    def create(self, floor_plan_aptcast_id, *args, **kwargs):
+        self.action = "{0}/{1}".format(floor_plan_aptcast_id, self.base_action)
+
+        data = self.build_unit(*args, **kwargs)
 
         return self.api.post(self.get_app(), self.get_action(), params=data)
 
-    def update(self, floor_plan_aptcast_id, unit_aptcast_id, number, building,
-               floor, available_date, description, rent_low, rent_high,
-               deposit_low, deposit_high, square_feet_low, square_feet_high):
-
+    def update(self, floor_plan_aptcast_id, unit_aptcast_id, *args, **kwargs):
         self.action = "{0}/{1}/{2}".format(
             floor_plan_aptcast_id, self.base_action, unit_aptcast_id)
 
-        description_dict = None
-        if description:
-            description_dict = {
-                "body": description
-            }
-
-        data = {
-            "number": number,
-            "building": building,
-            "floor": floor,
-            "available_date": available_date,
-            "description": description_dict,
-            "rent": {
-                "low": rent_low,
-                "high": rent_high
-            },
-            "deposit": {
-                "low": deposit_low,
-                "high": deposit_high
-            },
-            "square_feet": {
-                "low": square_feet_low,
-                "high": square_feet_high
-            }
-        }
+        data = self.build_unit(*args, **kwargs)
 
         return self.api.put(self.get_app(), self.get_action(), params=data)
 
